a6eaff316aab7b7bc167ead0421f9889715725e3
jack.adase <backjdase@gmail.com>
2020-05-18 14:05:58 -0400

    - Added: Wrought Iron Fence, Casting Table Components, Recipes

---
+++ src/main/java/net/mcreator/coincraft/block/BlockWroughtIronFence.java
@@ -0,0 +1,60 @@
+
+package net.mcreator.coincraft.block;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockFence;
+import net.minecraft.block.Block;
+
+import net.mcreator.coincraft.creativetab.TabCoinCraft;
+import net.mcreator.coincraft.ElementsCoinCraft;
+
+@ElementsCoinCraft.ModElement.Tag
+public class BlockWroughtIronFence extends ElementsCoinCraft.ModElement {
+	@GameRegistry.ObjectHolder("coincraft:wroughtironfence")
+	public static final Block block = null;
+	public BlockWroughtIronFence(ElementsCoinCraft instance) {
+		super(instance, 44);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("wroughtironfence"));
+		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
+				new ModelResourceLocation("coincraft:wroughtironfence", "inventory"));
+	}
+	public static class BlockCustom extends BlockFence {
+		public BlockCustom() {
+			super(Material.IRON, Material.IRON.getMaterialMapColor());
+			setUnlocalizedName("wroughtironfence");
+			setSoundType(SoundType.METAL);
+			setHarvestLevel("pickaxe", 3);
+			setHardness(5F);
+			setResistance(30F);
+			setLightLevel(0F);
+			setLightOpacity(0);
+			setCreativeTab(TabCoinCraft.tab);
+		}
+
+		@Override
+		public boolean isOpaqueCube(IBlockState state) {
+			return false;
+		}
+	}
+}
