bb4351860a25350bd5f45ec2ebee928f20e37404
jack.adase <backjdase@gmail.com>
2020-05-18 11:10:56 -0400

    - Added Crucible Components + Recipes

---
+++ src/main/java/net/mcreator/coincraft/item/ItemSpigotMount.java
@@ -0,0 +1,60 @@
+
+package net.mcreator.coincraft.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import net.mcreator.coincraft.creativetab.TabCraftedComponents;
+import net.mcreator.coincraft.ElementsCoinCraft;
+
+@ElementsCoinCraft.ModElement.Tag
+public class ItemSpigotMount extends ElementsCoinCraft.ModElement {
+	@GameRegistry.ObjectHolder("coincraft:spigotmount")
+	public static final Item block = null;
+	public ItemSpigotMount(ElementsCoinCraft instance) {
+		super(instance, 39);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("coincraft:spigotmount", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 1;
+			setUnlocalizedName("spigotmount");
+			setRegistryName("spigotmount");
+			setCreativeTab(TabCraftedComponents.tab);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+	}
+}
