629cf00b0243a6d7d2266ffa041d38ecd135c3cb
BuildTools <unconfigured@null.spigotmc.org>
2020-05-20 19:12:48 -0400

    -added cast iron furnace (cif) json and tex -added cif gui -began work on procedures for cif

---
+++ src/main/java/net/mcreator/coincraft/block/BlockCastIronFurnaceOn.java
@@ -0,0 +1,326 @@
+
+package net.mcreator.coincraft.block;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.tileentity.TileEntityLockableLoot;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.ItemStackHelper;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.inventory.Container;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.EnumPushReaction;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.BlockHorizontal;
+import net.minecraft.block.Block;
+
+import net.mcreator.coincraft.procedure.ProcedureCastIronFurnaceUpdateTick;
+import net.mcreator.coincraft.gui.GuiCastIronFurnaceGUI;
+import net.mcreator.coincraft.creativetab.TabCoinCraft;
+import net.mcreator.coincraft.ElementsCoinCraft;
+import net.mcreator.coincraft.CoinCraft;
+
+import java.util.Random;
+
+@ElementsCoinCraft.ModElement.Tag
+public class BlockCastIronFurnaceOn extends ElementsCoinCraft.ModElement {
+	@GameRegistry.ObjectHolder("coincraft:castironfurnaceon")
+	public static final Block block = null;
+	public BlockCastIronFurnaceOn(ElementsCoinCraft instance) {
+		super(instance, 59);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("castironfurnaceon"));
+		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+		GameRegistry.registerTileEntity(TileEntityCustom.class, "coincraft:tileentitycastironfurnaceon");
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
+				new ModelResourceLocation("coincraft:castironfurnaceon", "inventory"));
+	}
+	public static class BlockCustom extends Block implements ITileEntityProvider {
+		public static final PropertyDirection FACING = BlockHorizontal.FACING;
+		public BlockCustom() {
+			super(Material.IRON);
+			setUnlocalizedName("castironfurnaceon");
+			setSoundType(SoundType.METAL);
+			setHarvestLevel("pickaxe", 0);
+			setHardness(5F);
+			setResistance(30F);
+			setLightLevel(1F);
+			setLightOpacity(0);
+			setCreativeTab(TabCoinCraft.tab);
+			this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+		}
+
+		@Override
+		public int tickRate(World world) {
+			return 1;
+		}
+
+		@Override
+		protected net.minecraft.block.state.BlockStateContainer createBlockState() {
+			return new net.minecraft.block.state.BlockStateContainer(this, new IProperty[]{FACING});
+		}
+
+		@Override
+		public IBlockState withRotation(IBlockState state, Rotation rot) {
+			return state.withProperty(FACING, rot.rotate((EnumFacing) state.getValue(FACING)));
+		}
+
+		@Override
+		public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
+			return state.withRotation(mirrorIn.toRotation((EnumFacing) state.getValue(FACING)));
+		}
+
+		@Override
+		public IBlockState getStateFromMeta(int meta) {
+			return this.getDefaultState().withProperty(FACING, EnumFacing.getFront(meta));
+		}
+
+		@Override
+		public int getMetaFromState(IBlockState state) {
+			return ((EnumFacing) state.getValue(FACING)).getIndex();
+		}
+
+		@Override
+		public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta,
+				EntityLivingBase placer) {
+			return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+		}
+
+		@Override
+		public boolean isOpaqueCube(IBlockState state) {
+			return false;
+		}
+
+		@Override
+		public EnumPushReaction getMobilityFlag(IBlockState state) {
+			return EnumPushReaction.BLOCK;
+		}
+
+		@Override
+		public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+			return new ItemStack(BlockCastIronFurnaceOff.block, (int) (1));
+		}
+
+		@Override
+		public MapColor getMapColor(IBlockState state, IBlockAccess blockAccess, BlockPos pos) {
+			return MapColor.IRON;
+		}
+
+		@Override
+		public boolean canSilkHarvest(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
+			return false;
+		}
+
+		@Override
+		public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+			drops.add(new ItemStack(BlockCastIronFurnaceOff.block, (int) (1)));
+		}
+
+		@Override
+		public TileEntity createNewTileEntity(World worldIn, int meta) {
+			return new TileEntityCustom();
+		}
+
+		@Override
+		public boolean eventReceived(IBlockState state, World worldIn, BlockPos pos, int eventID, int eventParam) {
+			super.eventReceived(state, worldIn, pos, eventID, eventParam);
+			TileEntity tileentity = worldIn.getTileEntity(pos);
+			return tileentity == null ? false : tileentity.receiveClientEvent(eventID, eventParam);
+		}
+
+		@Override
+		public EnumBlockRenderType getRenderType(IBlockState state) {
+			return EnumBlockRenderType.MODEL;
+		}
+
+		@Override
+		public void breakBlock(World world, BlockPos pos, IBlockState state) {
+			TileEntity tileentity = world.getTileEntity(pos);
+			if (tileentity instanceof TileEntityCustom)
+				InventoryHelper.dropInventoryItems(world, pos, (TileEntityCustom) tileentity);
+			world.removeTileEntity(pos);
+			super.breakBlock(world, pos, state);
+		}
+
+		@Override
+		public boolean hasComparatorInputOverride(IBlockState state) {
+			return true;
+		}
+
+		@Override
+		public int getComparatorInputOverride(IBlockState blockState, World worldIn, BlockPos pos) {
+			TileEntity tileentity = worldIn.getTileEntity(pos);
+			if (tileentity instanceof TileEntityCustom)
+				return Container.calcRedstoneFromInventory((TileEntityCustom) tileentity);
+			else
+				return 0;
+		}
+
+		@Override
+		public void onBlockAdded(World world, BlockPos pos, IBlockState state) {
+			super.onBlockAdded(world, pos, state);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			world.scheduleUpdate(new BlockPos(x, y, z), this, this.tickRate(world));
+		}
+
+		@Override
+		public void updateTick(World world, BlockPos pos, IBlockState state, Random random) {
+			super.updateTick(world, pos, state, random);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			{
+				java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+				ProcedureCastIronFurnaceUpdateTick.executeProcedure($_dependencies);
+			}
+			world.scheduleUpdate(new BlockPos(x, y, z), this, this.tickRate(world));
+		}
+
+		@Override
+		public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer entity, EnumHand hand, EnumFacing direction,
+				float hitX, float hitY, float hitZ) {
+			super.onBlockActivated(world, pos, state, entity, hand, direction, hitX, hitY, hitZ);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			if (entity instanceof EntityPlayer) {
+				((EntityPlayer) entity).openGui(CoinCraft.instance, GuiCastIronFurnaceGUI.GUIID, world, x, y, z);
+			}
+			return true;
+		}
+	}
+
+	public static class TileEntityCustom extends TileEntityLockableLoot {
+		private NonNullList<ItemStack> stacks = NonNullList.<ItemStack>withSize(4, ItemStack.EMPTY);
+		@Override
+		public int getSizeInventory() {
+			return 4;
+		}
+
+		@Override
+		public boolean isEmpty() {
+			for (ItemStack itemstack : this.stacks)
+				if (!itemstack.isEmpty())
+					return false;
+			return true;
+		}
+
+		@Override
+		public boolean isItemValidForSlot(int index, ItemStack stack) {
+			if (index == 3)
+				return false;
+			return true;
+		}
+
+		@Override
+		public ItemStack getStackInSlot(int slot) {
+			return stacks.get(slot);
+		}
+
+		@Override
+		public String getName() {
+			return "container.castironfurnaceon";
+		}
+
+		@Override
+		public void readFromNBT(NBTTagCompound compound) {
+			super.readFromNBT(compound);
+			this.stacks = NonNullList.<ItemStack>withSize(this.getSizeInventory(), ItemStack.EMPTY);
+			if (!this.checkLootAndRead(compound))
+				ItemStackHelper.loadAllItems(compound, this.stacks);
+		}
+
+		@Override
+		public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+			super.writeToNBT(compound);
+			if (!this.checkLootAndWrite(compound))
+				ItemStackHelper.saveAllItems(compound, this.stacks);
+			return compound;
+		}
+
+		@Override
+		public SPacketUpdateTileEntity getUpdatePacket() {
+			return new SPacketUpdateTileEntity(this.pos, 0, this.getUpdateTag());
+		}
+
+		@Override
+		public NBTTagCompound getUpdateTag() {
+			return this.writeToNBT(new NBTTagCompound());
+		}
+
+		@Override
+		public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+			this.readFromNBT(pkt.getNbtCompound());
+		}
+
+		@Override
+		public void handleUpdateTag(NBTTagCompound tag) {
+			this.readFromNBT(tag);
+		}
+
+		@Override
+		public int getInventoryStackLimit() {
+			return 64;
+		}
+
+		@Override
+		public String getGuiID() {
+			return "coincraft:castironfurnaceon";
+		}
+
+		@Override
+		public Container createContainer(InventoryPlayer playerInventory, EntityPlayer playerIn) {
+			return new GuiCastIronFurnaceGUI.GuiContainerMod(this.getWorld(), this.getPos().getX(), this.getPos().getY(), this.getPos().getZ(),
+					playerIn);
+		}
+
+		@Override
+		protected NonNullList<ItemStack> getItems() {
+			return this.stacks;
+		}
+	}
+}
