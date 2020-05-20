34a67c71c91e262b0448cf8d05cbe9dea93a8b3f
BuildTools <unconfigured@null.spigotmc.org>
2020-05-20 19:23:20 -0400

    -fixed naming error for cif off

--- src/main/java/net/mcreator/coincraft/block/BlockCastIronFurnaceOn.java
+++ src/main/java/net/mcreator/coincraft/block/BlockCastIronFurnaceOn.java
@@ -18,6 +18,7 @@ import net.minecraft.util.Mirror;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.tileentity.TileEntityLockableLoot;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
@@ -90,6 +91,12 @@ public class BlockCastIronFurnaceOn extends ElementsCoinCraft.ModElement {
 			setLightOpacity(0);
 			setCreativeTab(TabCoinCraft.tab);
 			this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+		}
+
+		@SideOnly(Side.CLIENT)
+		@Override
+		public BlockRenderLayer getBlockLayer() {
+			return BlockRenderLayer.CUTOUT;
 		}
 
 		@Override
